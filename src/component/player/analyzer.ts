1a428c579514b95b13301a06080e6f4d1fb4ca90
NIIKURA Shozo <niikura@nyam.co.jp>
2025-01-09 03:55:55 +0900

    feat: 適当にフラグを立てる機能を追加しました。

--- src/component/player/analyzer.ts
+++ src/component/player/analyzer.ts
@@ -1,3 +1,5 @@
+import { COVERED, FLAG, FLAGGED, NOT_OPEN } from "../../lib/mskai/constants";
+
 interface gameInfo {
   cols: number;
   rows: number;
@@ -7,6 +9,9 @@ export const analyzeBtnClicked = (props: gameInfo) => {
   const {cols, rows} = props;
   const analyzer = new Analyzer(cols, rows);
   console.log("ANLYZING2...", analyzer);
+  const bombs = analyzer.searchBombs();
+  console.log(bombs);
+  analyzer.markFlags(bombs);
 };
 
 
@@ -25,8 +30,43 @@ class Analyzer {
     this.read();
   }
 
+  public searchBombs(): number[] {
+    this.read();
+    if (this.allCovered()) return [];
+
+    const coveredTiles = this.coveredTiles();
+    return coveredTiles.map(idx => {
+      if (this.tiles[idx] === COVERED && Math.random() * 10 > 5) return idx
+    }).filter(idx => idx !== undefined);
+  }
+
+  public markFlags(indecies: number[]) {
+    const elements = this.tileElements();
+    indecies.forEach(idx => {
+      elements[idx].dispatchEvent(new MouseEvent('contextmenu', { bubbles: true, cancelable: true }))
+    });
+  }
+
+  // 一つも開いていない場合は true
+  private allCovered(): boolean {
+    return this.tiles.reduce((prev, current) => prev && (current == COVERED || current == FLAG), true);
+  }
+
+  // 開いていないタイルのIndexを全て返す
+  private coveredTiles(): number[] {
+    return this.tiles.map((tile, idx) => {
+      if (tile == COVERED) {
+        return idx;
+      }
+    }).filter(idx => idx !== undefined);
+  }
+
+  private tileElements(): NodeListOf<HTMLImageElement> {
+    return document.querySelectorAll('div[data-id="board"] img');
+  }
+
   public read() {
-    const imgs = document.querySelectorAll('div[data-id="board"] img');
+    const imgs = this.tileElements();
     this.tiles = Array.from(imgs).map((node) => parseInt(node.getAttribute("data-tile") || '0'));
   }
 
