92f7d86fa6879b0614e060350897112960411fae
NIIKURA Shozo <niikura@nyam.co.jp>
2025-01-12 01:20:53 +0900

    feat: 極みレベルの縦と横を入れ替えました。

--- src/lib/mskai/level.ts
+++ src/lib/mskai/level.ts
@@ -21,5 +21,6 @@ export const LEVELS: KeyLevelMap = {
     [EASY]: { cols: 8, rows: 8, bombs: 8, level: EASY },
     [MEDIUM]: { cols: 16, rows: 16, bombs: 45, level: MEDIUM },
     [HARD]: { cols: 30, rows: 16, bombs: 90, level: HARD },
-    [EXTREME]: { cols: 48, rows: 68, bombs: 777, level: EXTREME },
+    // [EXTREME]: { cols: 48, rows: 68, bombs: 777, level: EXTREME },
+    [EXTREME]: { cols: 68, rows: 48, bombs: 777, level: EXTREME },
 }
